5251d51eccd918deb5bf07e1dfd327c22803aa0e
odetv <abdiutama02@gmail.com>
2023-06-20 09:23:02 +0800

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Projects
+                  Module
                 </a>
 
                 <a
@@ -125,7 +125,7 @@ const Header = () => {
                 href="#"
                 className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
               >
-                Projects
+                Module
               </a>
 
               <a
