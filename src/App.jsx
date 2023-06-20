702fcdbab96b3e3730b19af08d35ad0936388c70
odetv <abdiutama02@gmail.com>
2023-06-20 08:10:56 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1 className="text-center text-2xl mt-4">Konten</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
