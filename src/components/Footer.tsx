86db8c26faccaef2a59ee5b8465bff13c47f13d8
odetv <abdiutama02@gmail.com>
2023-06-20 14:35:49 +0800

    updated page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="flex flex-col h-screen justify-end">
             <div className="bg-slate-100 py-6 text-xs text-center dark:text-gray-400">
-                © 2023 Team Undiksha. All rights reserved.
+                © 2023 Tim CVPD Undiksha. All rights reserved.
             </div>
         </footer>
     );
