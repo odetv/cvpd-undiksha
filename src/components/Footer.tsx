b0d8074a588837b1da202cc27930fd9ec0851e1a
odetv <abdiutama02@gmail.com>
2023-06-20 10:26:33 +0800

    update page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="flex flex-col h-screen justify-end">
-      <div className="bg-slate-100 py-6 text-xs text-center dark:text-gray-400">
-        © 2023 Team Undiksha. All rights reserved.
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="flex flex-col h-screen justify-end">
+            <div className="bg-slate-100 py-6 text-xs text-center dark:text-gray-400">
+                © 2023 Team Undiksha. All rights reserved.
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
