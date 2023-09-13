aa228874161f3a49516b1c20348c397833a24ec2
odetv <abdiutama02@gmail.com>
2023-09-13 21:17:40 +0800

    updated

--- src/pages/section1/S1P1.tsx
+++ src/pages/section1/S1P1.tsx
@@ -9,20 +9,17 @@ const S1P1 = () => {
     const ContentS1P1 = [
         {
             id: "",
-            title: "Section I",
-            subtitle: "CVPD THAT ATTACK CITRUS PRODUCTION",
-            content1: "Citrus as an Important Commodity",
-            content2:
-                "Citrus is one of the most important fruit commodities in Indonesia. Citrus came in second place behind bananas in terms of fruit production in 2017. People's consumption reached 0.06 kg per week in 2015 and increased up to 0.09 kg per week in 2016. Citrus also ranked first for the average fruit expenditure of Indonesians (in rupiah), both in the village and in the city (Ministry of Agriculture in Indonesia, 2019). Citrus consumption tends to increase, which is caused by the many good nutrients and non-nutrients found in this fruit. Citrus contains a number of secondary metabolites, such as flavonoids, alkaloids, coumarins, limonoids, carotenoids, phenolic acids, and essential oils, amongst several others (Figure 1). These active secondary metabolites show several biological activities of importance to human health, including anti-oxidative, antiinflammatory, and anti-cancer effects, as well as cardiovascular protective effects and neuroprotective effects (Adenaike & Abakpa, 2021).",
-            content3:
-                "With high demand for citrus consumption, this fruit plantation will be very profitable, increasing people's income, job opportunities, and national export foreign exchange. In terms of international trade, Indonesia has higher import than export values. Among South East Asian countries, Indonesia was considered the second largest citrus importer after Malaysia, with an increasing trend of up to 11% per year (Taufiq, et al., 2019 ). Citrus is also an important horticultural commodity that is growing rapidly in Bali. According to Alitawan (2017), Kintamani, Bangli is the center of the highest citrus production in Bali, with citrus land areas increasing in 2015. Many local farmers in Kintamani are converting land they have traditionally used to harvest corn into citrus-producing plots. They divert corn crop land into land where they plant citrus to sell in the local market, to meet the demands of tourists, the local society, and ritual purposes (Figure 2).",
-            labelFigure1: "Figure 1: Nutrients in Citrus",
-            labelFigure2: "Figure 2: Citrus use as a ritual",
-            labelBack: "Back",
-            labelNext: "Next",
-            textBack: "ENCOUNTER FOCAL ISSUE",
-            textNext:
-                "Citrus Vein Phloem Degeneration that Attack Citrus Production",
+            title: t("1"),
+            subtitle: t("2"),
+            content1: t("3"),
+            content2: t("4"),
+            content3: t("5"),
+            labelFigure1: t("6"),
+            labelFigure2: t("7"),
+            labelBack: t("labelBack"),
+            labelNext: t("labelNext"),
+            textBack: t("textBack"),
+            textNext: t("textNext"),
             linkBack: "/section1",
             linkNext: "/section1/2",
         },
